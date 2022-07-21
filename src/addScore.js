8a218a9f45f18113df5aa6d3dbc977beeea7bbad
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-07-21 12:26:07 +0100

    add styling

--- src/addScore.js
+++ src/addScore.js
@@ -2,9 +2,15 @@ const scoreList = document.querySelector('.score-list');
 
 const addScore = (name, score) => {
   const li = document.createElement('li');
-  li.innerHTML = `${name} : ${score}`;
+  li.innerHTML = ` 👤 ${name} : ${score}`;
   scoreList.appendChild(li);
-
+  if (score >= 1000) {
+    li.innerHTML = `👤 ⭐⭐⭐ ${name} : ${score}`;
+  } else if (score >= 500 && score < 1000) {
+    li.innerHTML = `👤 ⭐⭐ ${name} : ${score}`;
+  } else if (score < 500) {
+    li.innerHTML = `👤 ⭐ ${name} : ${score}`;
+  }
   const index = [...document.querySelectorAll('li')].length;
   if (index % 2 === 0) {
     li.classList.add('even');
