1cb199a50e4323f83cccbe8bba5306f4d5923a90
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-25 14:30:26 +0100

    Update index.js
    Removed unused code

--- src/index.js
+++ src/index.js
@@ -14,12 +14,3 @@ refreshBtn.addEventListener('click', () => {
   displayScores();
 });
 
-// fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games', {
-//   method: 'POST',
-//   body: JSON.stringify({ name: 'Name of the game' }),
-//   headers: {
-//     'Content-Type': 'application/json; charset=UTF-8',
-//   },
-// })
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
